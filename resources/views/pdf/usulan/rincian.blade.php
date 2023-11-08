8a93ebcbbeda44a1ff502a315c104e95bafb902a
124f5411 <ophandelta62@gmail.com>
2023-11-08 21:00:00 +0900

    custom pantau usulan & input usulan

--- resources/views/pdf/usulan/rincian.blade.php
+++ resources/views/pdf/usulan/rincian.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SSH {{ $tahun }}</title>
+    <title>USULAN {{ $tahun }}</title>
     <link href="{{ asset('themes/css/sb-admin-2.min.css') }}" rel="stylesheet">
     <style>
         .table td, .table th {
